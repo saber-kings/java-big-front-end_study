2bea96601dd1a94a4739a737978742728cfa6838
saber-kings <luanzhaofei@outlook.com>
2020-04-21 00:34:53 +0800

    修复使用了lombok后不能添加商品问题，以及优化了修改和删除的方法

--- milk_tea/src/com/saberking/pojo/Good.java
+++ milk_tea/src/com/saberking/pojo/Good.java
@@ -17,4 +17,36 @@ public class Good implements Serializable {
 	private String gStuff;
 	private String gDesc;
 
+//	public String getgName() {
+//		return gName;
+//	}
+//
+//	public void setgName(String gName) {
+//		this.gName = gName;
+//	}
+//
+//	public int getgPrice() {
+//		return gPrice;
+//	}
+//
+//	public void setgPrice(int gPrice) {
+//		this.gPrice = gPrice;
+//	}
+//
+//	public String getgStuff() {
+//		return gStuff;
+//	}
+//
+//	public void setgStuff(String gStuff) {
+//		this.gStuff = gStuff;
+//	}
+//
+//	public String getgDesc() {
+//		return gDesc;
+//	}
+//
+//	public void setgDesc(String gDesc) {
+//		this.gDesc = gDesc;
+//	}
+
 }
